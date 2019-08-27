10e4b6833deab3ce5ed09c843842d80e0811a207
麋鹿BRO <807424100@qq.com>
2019-08-27 22:57:24 +0800

    hashmap

---
+++ src/main/java/javabase/HashMapLearn.java
@@ -0,0 +1,14 @@
+package javabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * HashMap原理
+ */
+public class HashMapLearn {
+    public static void main(String[] args) {
+        Map map = new HashMap<>();
+        map.put("test", "test001");
+    }
+}
